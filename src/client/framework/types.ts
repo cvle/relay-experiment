c9656a4019072e233c6905ddf0f5ec7d2280de9a
Chi Vinh Le <vinh@wikiwi.io>
2018-06-12 17:50:59 +0200

    Add a comment about types

--- src/client/framework/types.ts
+++ src/client/framework/types.ts
@@ -1,1 +1,2 @@
+// TODO: Extract useful common types into its own package.
 export { Diff, Omit, Overwrite } from "talk-ui/types";
--- src/client/ui/types.ts
+++ src/client/ui/types.ts
@@ -1,3 +1,5 @@
+// TODO: Extract useful common types into its own package.
+
 export type Diff<T extends keyof any, U extends keyof any> = ({ [P in T]: P } &
   { [P in U]: never } & { [x: string]: never })[T];
 
