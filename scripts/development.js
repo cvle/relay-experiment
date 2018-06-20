fc1a1bfc003316b130e6b32d3dda52cc5cfc3b60
okbel <curciobelen@gmail.com>
2018-06-20 17:21:06 -0300

    Working first iteration

--- scripts/development.js
+++ scripts/development.js
@@ -1,49 +1,82 @@
-'use strict';
-
 const chalk = require('chalk');
 const spawn = require('cross-spawn');
-const program = require('commander');
+const commander = require('commander');
 const { runWatcher } = require('./watcher');
 const packageJson = require('../package.json');
+const log = console.log;
 
-program
-  .version(packageJson.version)
-  .option('--client-path <path>', 'change the client directory')
-  .option('--server-path <path>', 'change the server directory');
+function init() {
+  const program = new commander.Command(packageJson.version);
 
-program
-  .command('dev')
-  .description('runs watcher for client')
-  // .option('--dev', 'print additional logs')
-  .action(async function(env, options) {
-    // install dependencies
-    await installDependencies();
-    // run watceher
-    // perform more tasks if needed
-    runWatcher();
+  program
+    .command('setup')
+    .description('install dependencies and setup')
+    .action(async () => {
+      await clean();
+      await installDependencies();
+      // perform more tasks if needed, migrations, reconciliation, etc.
+    });
+
+  program
+    .command('watch')
+    .option('--no-install', 'performs an install before running the watcher')
+    .description('runs watcher for client')
+    .action(async (opts) => {
+      await clean();
+
+      if (opts.install) {
+        // install dependencies
+        await installDependencies();
+      }
+
+      // run watceher
+      runWatcher();
+      // perform more tasks if needed
+    });
+
+  program.parse(process.argv);
+}
+
+function clean() {
+  return new Promise((resolve, reject) => {
+    // install all the dependencies before running the watcher
+    const command = 'rm';
+    const args = ['-rf', 'dist'];
+
+    const child = spawn(command, args, { stdio: 'inherit' });
+    child.on('close', (code) => {
+      if (code !== 0) {
+        log(chalk.red('We had an error while cleaning', code));
+        reject({
+          command: `${command} ${args.join(' ')}`,
+        });
+        return;
+      }
+      log(chalk.green('Cleanup done.'));
+      resolve();
+    });
   });
-
-program.parse(process.argv);
+}
 
 function installDependencies() {
   return new Promise((resolve, reject) => {
-    // Install all the dependencies before running the watcher
+    // install all the dependencies before running the watcher
     const command = 'npm';
     const args = ['install'];
 
     const child = spawn(command, args, { stdio: 'inherit' });
     child.on('close', (code) => {
       if (code !== 0) {
-        console.log(
-          chalk.red('We had an error installing the dependencies.', code)
-        );
+        log(chalk.red('We had an error installing the dependencies.', code));
         reject({
           command: `${command} ${args.join(' ')}`,
         });
         return;
       }
-      console.log(chalk.green('All the dependecies have been installed.'));
+      log(chalk.green('All the dependecies have been installed.'));
       resolve();
     });
   });
 }
+
+init();
--- scripts/watcher.js
+++ scripts/watcher.js
@@ -2,9 +2,10 @@ const path = require('path');
 const chalk = require('chalk');
 const spawn = require('cross-spawn');
 const watchman = require('fb-watchman');
+const log = console.log;
 
+// Directory to watch
 const dir = path.resolve(__dirname, '../src');
-const validExtensions = ['*.tsx'];
 
 function runWatcher() {
   const client = new watchman.Client();
@@ -13,21 +14,22 @@ function runWatcher() {
     { optional: [], required: ['relative_root'] },
     function(error, resp) {
       if (error) {
-        console.log(error);
+        log(chalk.red('Error in the capability check:', error));
         client.end();
         return;
       }
 
       // Initiate the watch
       client.command(['watch-project', dir], function(error, resp) {
+        // Handling errors
         if (error) {
-          console.log(chalk.yellow('Error initiating watch:', error));
+          log(chalk.red('Error initiating watch:', error));
           return;
         }
 
         // Handling warnings
         if ('warning' in resp) {
-          console.log(chalk.red('Error initiating watch:', resp.warning));
+          log(chalk.yellow('Error initiating watch:', resp.warning));
         }
 
         // Watch established, building subscription
@@ -37,35 +39,61 @@ function runWatcher() {
   );
 }
 
-function buildSubscription(client, watch, relative_path) {
-  sub = {
-    // Match the extensions
-    expression: ['allof', ['match'].concat(validExtensions)],
-    // Which fields we're interested in
-    fields: ['name', 'size', 'mtime_ms', 'exists', 'type'],
-  };
+function compile() {
+  // install all the dependencies before running the watcher
+  const command = 'npm';
+  const args = ['run', 'compile:relay:stream'];
 
-  if (relative_path) {
-    sub.relative_root = relative_path;
-  }
+  const child = spawn(command, args, { stdio: 'inherit' });
 
-  client.command(['subscribe', watch, 'watcher', sub], function(error, resp) {
-    if (error) {
-      // The subscription failed
-      console.log(chalk.yellow('Failed to subscribe', error));
-      return;
-    }
-    console.log('subscription ' + resp.subscribe + ' established');
+  child.on('data', (data) => {
+    log(chalk.grey('buildClient', data));
   });
 
-  client.on('subscription', function(resp) {
-    if (resp.subscription !== 'watcher') return;
+  child.on('close', (code) => {
+    if (code !== 0) {
+      log(chalk.red('We had an error building the client', code));
+      return;
+    }
+  });
+}
 
-    resp.files.forEach(function(file) {
-      // Converts Int64 instance to javascript integer
-      const mtime_ms = +file.mtime_ms;
+function buildSubscription(client, watch, relative_path) {
+  client.command(['clock', watch], function(error, resp) {
+    if (error) {
+      console.error('Failed to query clock:', error);
+      return;
+    }
 
-      console.log('File changed: ' + file.name, mtime_ms);
+    sub = {
+      // Match any `.js` file in the dir_of_interest
+      expression: ['anyof', ['match', '*.ts'], ['match', '*.tsx']],
+      // Which fields we're interested in
+      fields: ['name', 'size', 'exists', 'type'],
+      // add our time constraint
+      since: resp.clock,
+    };
+
+    if (relative_path) {
+      sub.relative_root = relative_path;
+    }
+
+    client.command(['subscribe', watch, 'watcher', sub], function(error, resp) {
+      if (error) {
+        // The subscription failed
+        log(chalk.red('Failed to subscribe \n', error));
+        return;
+      }
+      log(chalk.green('Subscription ' + resp.subscribe + ' established.'));
+    });
+
+    client.on('subscription', function(resp) {
+      if (resp.subscription !== 'watcher') return;
+
+      resp.files.forEach(function(file) {
+        console.log('file changed: ' + file.name);
+        compile();
+      });
     });
   });
 }
